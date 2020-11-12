c9a78a91220439ccfc1ca1c373eb92d5856b4f72
Pablito <pablovzqz768@gmail.com>
2020-11-12 12:46:14 -0300

    agregando nuevo modelo

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+    public Camera FPSCamara;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    float H;
+    float V;
+
+    void Start () {
+
+    }
+    
+
+    void Update()
+    {
+
+        H = horizontalSpeed * Input.GetAxis("Mouse X");
+        V = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, H, 0);
+        FPSCamara.transform.Rotate(-V,0,0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,0,0.1f);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.1f,0,0);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0,0,-0.1f);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.1f,0,0);
+        }
+	}
+}
