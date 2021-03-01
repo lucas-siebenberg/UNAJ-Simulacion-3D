eb90b87bda24c07f519cfbcd96192fd615b2bee0
Pablito <pablovzqz768@gmail.com>
2021-03-01 12:59:47 -0300

    se genera escena de menu principal

---
+++ Assets/Scripts/CameraMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMenu : MonoBehaviour
+{
+public float velocidad = 0.75f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,1,0)*velocidad);
+    }
+}
